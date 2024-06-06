e0edd6928c9b32c4060dfabcfa468816619cdacd
Văn Thiết <tretrauthiet@gmail.com>
2024-06-06 11:59:54 +0700

    change

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,10 @@
-import { useState } from "react";
-import Click from "./components/button/click";
+
 const App = () => {
-  const [count , setCount] = useState(0)
-  const clickToBtn = ()=>{
-    setCount(count + 1)
-  }
-  const clickBtn = ()=>{
-    console.log('cc');
-  }
+
 
   return (
     <div>
-    {count}
-      <Click clickBtn={clickBtn}/>
-       <button onClick={clickToBtn}>Count</button>
+   <h1>React 19</h1>
     </div>
   );
 };
